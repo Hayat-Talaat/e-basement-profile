f1a791bb3e29cfecdee3fbeea081664f1695f6e0
hayatTalaat <h.talaat@xware.co>
2018-10-10 23:21:08 +0200

    add 3 component for bartering (Accept - Refused - In Progress)

---
+++ src/app/Profile/Bartering-Refused/bartering-refused.component.spec.ts
@@ -0,0 +1,25 @@
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { AppMyproduct } from './app-my-product.component';
+
+// describe('ProductsComponent', () => {
+//   let component: AppMyproduct;
+//   let fixture: ComponentFixture<AppMyproduct>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ AppMyproduct ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(AppMyproduct);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });

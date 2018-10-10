83c0200045f373606b0f245af887d09e86f98645
Ayman Abo-El-Abbas <ayman.abbass61@gmail.com>
2018-10-10 16:03:35 +0200

    app-my-product error
    app-my-product error

---
+++ src/app/Profile/app-notification/app-notification.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import 'src/assets/js/script.js'
+
+@Component({
+  selector: 'app-notification',
+  templateUrl: './app-notification/app-my-product.component.html',
+  styleUrls: ['./app-notification/app-my-product.component.css']
+})
+export class AppNotification implements OnInit {
+
+  constructor() {
+    
+   }
+
+  ngOnInit() {
+  }
+
+}

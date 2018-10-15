08dd64b9c083fd407ef8005da2aad078820e3bcc
hayatTalaat <h.talaat@xware.co>
2018-10-15 07:55:31 +0200

    Credit

--- src/app/Profile/Credit/Credit.component.ts
+++ src/app/Profile/Credit/Credit.component.ts
@@ -3,7 +3,8 @@ import 'src/assets/js/script.js'
 
 @Component({
   selector: 'app-credit',
-  templateUrl: './Credit.component.html'
+  templateUrl: './Credit.component.html',
+  styleUrls: ['./credit.component.css']
 })
 export class AppCredit implements OnInit {
 
